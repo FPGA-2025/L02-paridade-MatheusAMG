0679ca7db4e03929b89300a51e09447d22f9650e
Matheus <matheusamg@gmail.com>
2025-04-03 20:34:38 -0300

    primeiro teste

--- injetor.v
+++ injetor.v
@@ -5,6 +5,27 @@ module injetor(
   output reg [8:0] saida
 );
 
-// insira seu código aqui
+reg [8:0] dado_corrompido;
+wire erro_saida;
+
+always @(*) begin // injetar erro
+ 
+  if erro begin // se erro 1 entao injeta
+    assign dado_corrompido = entrada ^ (1 << n); // XOR 1 inverte o bit entao desloco o bit 1 para posicao e faco XOR nele
+  end else begin
+    assign dado_corrompido = entrada; // Nao injeta erro
+  end
+  
+end
+
+// Verifica paridade
+calcula_paridade u1 (
+  .dado(dado_corrompido),
+  .erro(erro_saida)
+);
+
+// Nao teria que ter um outra saida erro??
+
+
 
 endmodule
